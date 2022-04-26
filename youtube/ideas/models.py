cc78ce817d1a605e42cc1e3727427d36998eee41
Roman A <romanafonin@protonmail.com>
2022-04-26 21:52:27 +0200

    Registering models for admin site panel

--- youtube/ideas/models.py
+++ youtube/ideas/models.py
@@ -11,7 +11,7 @@ IDEA_STATUS = (
 class Idea(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    youtube_url = models.URLField()
+    youtube_url = models.URLField(null=True, blank=True)
     status = models.CharField(choices=IDEA_STATUS, max_length=30, default='pending...')
 
 
