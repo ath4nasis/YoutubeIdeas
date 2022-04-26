b31123e704eba189e42d1d91a5b678a5f5c0467d
Roman A <romanafonin@protonmail.com>
2022-04-26 21:05:00 +0200

    Creating db with basic models

--- youtube/ideas/models.py
+++ youtube/ideas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+IDEA_STATUS = (
+    ('pending', 'Waiting for review'),
+    ('accepted', 'Accepted'),
+    ('done', 'Done'),
+    ('rejected', 'Rejected')
+)
+
+
+class Idea(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    youtube_url = models.URLField()
+    status = models.CharField(choices=IDEA_STATUS, max_length=30, default='pending...')
+
+
+class Vote(models.Model):
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    reason = models.TextField()
