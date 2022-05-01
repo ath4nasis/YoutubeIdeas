862fe76cc32412936678920a90e6a1e5d2fb15a0
Roman A <romanafonin@protonmail.com>
2022-05-01 09:24:42 +0200

    adding extra columns to panel for better visibility

--- youtube/ideas/models.py
+++ youtube/ideas/models.py
@@ -14,6 +14,9 @@ class Idea(models.Model):
     youtube_url = models.URLField(null=True, blank=True)
     status = models.CharField(choices=IDEA_STATUS, max_length=30, default='pending...')
 
+    def __str__(self):
+        return self.title
+
 
 class Vote(models.Model):
     idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
